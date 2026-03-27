059cd1adb34326d0052db0c2d0479466e7beabdc
Tutorial <foditoon1@gmail.com>
2026-03-28 01:20:43 +0400

    docstring добавил для класса ожиданий

--- utils/waitUtils.py
+++ utils/waitUtils.py
@@ -8,11 +8,13 @@ class WaitUtils:
         self.driver = driver
 
     def wait_for_presence(self, element):
+        """Метод для ожидания наличия элемента на странице."""
         return WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located(element)
         )
 
     def wait_for_clickable(self, element):
+        """Метод для ожидания кликабельности элемента на странице."""
         return WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(element)
         )
