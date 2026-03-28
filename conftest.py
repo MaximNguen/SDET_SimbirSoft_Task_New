dc8125075ae34e6b159c24f43887e7065828bc60
Tutorial <foditoon1@gmail.com>
2026-03-28 11:50:50 +0400

    Немного изменил местоположение файла

--- conftest.py
+++ conftest.py
@@ -10,7 +10,6 @@ def driver():
     options = Options()
     
     options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
     
     driver  = webdriver.Chrome(options=options)
