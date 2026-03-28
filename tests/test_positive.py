750dcc323f3ad706e7f636197f4b9f47710b0a62
Tutorial <foditoon1@gmail.com>
2026-03-28 13:26:42 +0400

    Полные тест-кейс для позитивные случай

--- tests/test_positive.py
+++ tests/test_positive.py
@@ -51,6 +51,72 @@ class TestPositiveResult:
         self.main_page.send_longest()
         self.main_page.submit_click()
         
+        name = self.main_page.get_name_input().get_attribute("value")
+        password = self.main_page.get_password_input().get_attribute("value")
+        email = self.main_page.get_email_input().get_attribute("value")
+        message = self.main_page.get_message_input().get_attribute("value")
+        alert_state = self.main_page.check_alert()
+        alert_text = self.main_page.check_text_alert()
+        
+        with allure.step("Проверяем, что вставленные данные совпадают с данными в форме после отправки"):
+            assert name == test_data['name'], f"Имя не совпало, получили в поле - {name}"
+            assert password == test_data['password'], f"Пароль не совпал, получили в поле - {password}"
+            assert email == test_data['email'], f"Email не совпал, получили в поле - {email}"
+            assert message == test_data['expected_text_message'], f"Текст сообщения не совпал с ожидаемым, получили в поле - {message}"
+        
+        with allure.step("Достоверимся, что Alert выпал после отправки формы"):
+            assert alert_state, "Alert не выпал после нажатия Submit"
+            assert alert_text == test_data['expected_alert'], f"Alert не появился после отправки формы, получили - {alert_text}"
+            
+    @allure.story("Ввод только требующихся полей")
+    @allure.title("Проверка отправки формы со всеми требующимися полями")
+    @pytest.mark.parametrize("test_data", TestData.positive_required_only_data)
+    def test_fill_only_required(self, test_data):
+        allure.dynamic.description(
+            f"""
+            Тест проверяет отправку формы с заполнением только требующихся полей.
+            Данные теста:
+            - Имя: {test_data['name']}
+            """
+        )
+        
+        self.main_page.enter_name(test_data['name'])
+        self.main_page.submit_click()
+        
+        alert_text = self.main_page.check_text_alert()
+        name = self.main_page.get_name_input().get_attribute("value")
+        
+        with allure.step("Проверяем, что вставленные данные совпадают с данными в форме после отправки"):
+            assert name == test_data['name'], f"Имя не совпало, получили в поле - {name}"
+            
         with allure.step("Достоверимся, что Alert выпал после отправки формы"):
             assert self.main_page.check_alert(), "Alert не выпал после нажатия Submit"
-            assert self.main_page.check_text_alert() == test_data['expected_alert'], "Alert не появился после отправки формы"
+            assert alert_text == test_data['expected_alert'], f"Alert не появился после отправки формы, получили - {alert_text}"
+            
+    @allure.story("Неполный ввод полей, в том числе имени")
+    @allure.title("Проверка отправки формы с частично заполненными полями")
+    @pytest.mark.parametrize("test_data", TestData.positive_partial_form_data)
+    def test_fill_form_partial(self, test_data):
+        allure.dynamic.description(
+            f"""
+            Тест проверяет отправку формы с заполнением некоторых полей.
+            Данные теста:
+            - Имя: {test_data['name']}
+            """
+        )
+        
+        self.main_page.enter_name(test_data['name'])
+        self.main_page.send_longest()
+        self.main_page.submit_click()
+        
+        name = self.main_page.get_name_input().get_attribute("value")
+        message = self.main_page.get_message_input().get_attribute("value")
+        alert_text = self.main_page.check_text_alert()
+        
+        with allure.step("Проверяем, что вставленные данные совпадают с данными в форме после отправки"):
+            assert name == test_data['name'], f"Имя не совпало, получили в поле - {name}"
+            assert message == test_data['expected_text_message'], f"Текст сообщения не совпал с ожидаемым, получили в поле - {message}"
+            
+        with allure.step("Достоверимся, что Alert выпал после отправки формы"):
+            assert self.main_page.check_alert(), "Alert не выпал после нажатия Submit"
+            assert alert_text == test_data['expected_alert'], f"Alert не появился после отправки формы, получили - {alert_text}"
